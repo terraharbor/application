8896fa2d5f2e471a94b94b74ffa769e722eae134
fabricechapuis <fabrice.chapuis@heig-vd.ch>
2025-07-24 14:36:44 +0200

    feat(backend): impl registration & login

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,17 @@
+from typing import Annotated
 from fastapi import FastAPI, Request, Response, HTTPException, status, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.responses import FileResponse
+from user import User
+from hashlib import sha512
+from auth_functions import *
 import os, time, json
+from secrets import token_hex
+
 
 DATA_DIR = os.getenv("STATE_DATA_DIR", "./data")
 
-app = FastAPI(title="Terraform HTTP Backend Prototype")
+app = FastAPI(title="TerraHarbor")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -18,9 +24,47 @@ def _project_dir(name: str) -> str:
 def _latest_state(name: str) -> str:
     return os.path.join(_project_dir(name), "latest.tfstate")
 
+@app.post("/register")
+async def register(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Response:
+    """
+    Register a new user.
+    This endpoint creates a new user with the provided username, password, full name, and email.
+    The password is hashed using SHA-512.
+    """
+    if not form_data.username or not form_data.password:
+        raise HTTPException(status_code=400, detail="All fields are required")
+
+    user = User(username=form_data.username.strip(), disabled=True, sha512_hash=sha512(form_data.password.encode()).hexdigest(), usedforsecurity=True)
+    if user_exists(user):
+        raise HTTPException(status_code=400, detail="User already exists")
+    else:
+        register_user(user)
+        return {"message": "User registered successfully", "user": user}
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> dict:
+    """
+    Login endpoint to authenticate user and return a token.
+    This is a placeholder; actual implementation should use JWT or similar
+    """
+    user = get_user(form_data.username)
+
+    if not user or user.sha512_hash != sha512(form_data.password.encode()).hexdigest():
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+    
+    access_token = token_hex(32)
+
+    update_user_token(user.username, access_token)
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
 # GET  /state/{project}
 @app.get("/state/{name}", response_class=FileResponse)
-async def get_state(name: str) -> FileResponse:
+async def get_state(name: str, token: Annotated[str, Depends(oauth2_scheme)]) -> FileResponse:
+
+    if not is_token_valid(token):
+        raise HTTPException(status_code=401, detail="Invalid token")
+
     path = _latest_state(name)
     if not os.path.exists(path):
         raise HTTPException(status_code=404, detail="State not found")
@@ -28,7 +72,11 @@ async def get_state(name: str) -> FileResponse:
 
 # POST /state/{project}
 @app.post("/state/{name}", response_class=Response)
-async def put_state(name: str, request: Request) -> Response:
+async def put_state(name: str, request: Request, token: Annotated[str, Depends(oauth2_scheme)]) -> Response:
+    
+    if not is_token_valid(token):
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
     body = await request.body()
     if not body:
         raise HTTPException(status_code=400, detail="Empty body")
@@ -47,7 +95,11 @@ async def put_state(name: str, request: Request) -> Response:
 
 # LOCK  /state/{project}
 @app.api_route("/state/{name}", methods=["LOCK"], response_class=Response)
-async def lock_state(name: str, request: Request) -> Response:
+async def lock_state(name: str, request: Request, token: Annotated[str, Depends(oauth2_scheme)]) -> Response:
+
+    if not is_token_valid(token):
+        raise HTTPException(status_code=401, detail="Invalid token")
+    
     lock_path = os.path.join(_project_dir(name), ".lock")
     body = (await request.body()).decode() or "{}"
 
@@ -62,7 +114,11 @@ async def lock_state(name: str, request: Request) -> Response:
 
 # UNLOCK /state/{project}
 @app.api_route("/state/{name}", methods=["UNLOCK"], response_class=Response)
-async def unlock_state(name: str, request: Request) -> Response:
+async def unlock_state(name: str, request: Request, token: Annotated[str, Depends(oauth2_scheme)]) -> Response:
+
+    if not is_token_valid(token):
+        raise HTTPException(status_code=401, detail="Invalid token")
+
     lock_path = os.path.join(_project_dir(name), ".lock")
     if not os.path.exists(lock_path):
         # idempotent : ok even if the lock does not exist
@@ -82,7 +138,11 @@ async def unlock_state(name: str, request: Request) -> Response:
 
 # DELETE /state/{project}
 @app.delete("/state/{name}", response_class=Response)
-async def delete_state(name: str) -> Response:
+async def delete_state(name: str, token: Annotated[str, Depends(oauth2_scheme)]) -> Response:
+
+    if not is_token_valid(token):
+        raise HTTPException(status_code=401, detail="Invalid token")
+
     proj_dir = _project_dir(name)
     latest_path = _latest_state(name)
 
